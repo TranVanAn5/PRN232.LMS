6373ff8a8ccd243873fe68e1c6f547737770d2ec
TranVanAn5 <antvse181691@fpt.edu.vn>
2026-05-26 15:49:51 +0700

    cleanup: remove duplicate repositories and unused classes
    - Remove specific repository interfaces (ICourse, IEnrollment, etc)
    - Remove specific repository implementations (empty wrappers)
    - Remove Class1.cs placeholder
    - Remove duplicate Repositories folder structure
    - Keep only GenericRepository<T> + IGenericRepository<T>
    - Simplify project structure

--- PRN232.LMS.Repositories/Repositories/Interfaces/IStudentRepository.cs
+++
@@ -1,6 +0,0 @@
-using PRN232.LMS.Repositories.Entities;
-
-namespace PRN232.LMS.Repositories.Repositories.Interfaces
-{
-    public interface IStudentRepository : IGenericRepository<Student> { }
-}
--- PRN232.LMS.Service/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace PRN232.LMS.Service;
-
-public class Class1
-{
-
-}
